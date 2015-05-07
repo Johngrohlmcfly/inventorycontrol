efe12ba24c15554a6f36b273f7b343acd942893b
Sier Violencia <sierisimo@gmail.com>
2015-05-06 23:32:05 -0500

    Fixed issue using fgets()

--- src/papeleria.c
+++ src/papeleria.c
@@ -73,15 +73,17 @@ int main(void) {
 	printf("[RECEPCION DE PRODUCTOS]---------------------1\n");
 	printf("[BAJA DE PRODUCTOS]--------------------------2\n");
 	printf("[VENTA DE PRODUCTOS]-------------------------3\n\n");
+	printf("Su seleccion: ");
+
 	scanf("%d", &accion);
 
-	if(accion<0 || accion>3){
+	if(accion < 0 || accion > 3){
 		do {
 			count++;
 			printf("\nSeleccion invalida, ingresa un numero valido del menu....\n");
 			scanf("%d", &accion);
 
-			if(count==5){
+			if(count == 5){
 				printf("\nERROR EN LA EJECUCION, -----FIN DEL PROGRAMA-----\n\n");
 				break;
 			}
@@ -112,7 +114,6 @@ int main(void) {
 				continuar = getc(stdin);
 			} while(continuar != EOF && !(continuar == 'y' || continuar == 'Y' || continuar == 'n' || continuar == 'N'));
 
-
 			while(continuar == 'y' || continuar == 'Y'){
 				addproducts(posicion);
 
@@ -122,10 +123,8 @@ int main(void) {
 				posicion++;
 			}
 			break;
-
-
 		case 2:
-		continuar = 'n';
+			continuar = 'n';
 
 			downproducts(1);
 
@@ -134,48 +133,31 @@ int main(void) {
 				continuar = getc(stdin);
 			} while(continuar != EOF && !(continuar == 'y' || continuar == 'Y' || continuar == 'n' || continuar == 'N'));
 
-
 			while(continuar == 'y' || continuar == 'Y'){
 				downproducts(posicion);
 
 				printf("\nDesea dar de baja mas articulos??...(y/n)\n");
 				scanf("%c",&continuar);
 			}
-		break;
-
-
-
+			break;
 		case 3:
 			printf("\nHOLA MUNDO");
-		break;
-
+			break;
 	}
-
 	return 0;
 }
 
-
-
-
-
-
 //FUNCIONES y PROCEDIMIENTOS
 void addproducts(int a){
-
 	int i, mark, flag;
 
 	FILE *ptrFile;
 	ptrFile = fopen("papeleria.dat","ab");
 
-
-	if(ptrFile!=NULL){
-	 	fflush(stdin);
-
+	if(ptrFile != NULL){
 	 	printf("\nIntroduzca el nombre del producto: ");
-		//fgets(recepcion[a].nombre,sizeof(recepcion[a].nombre),stdin);
-		fgets(recepcion[a].nombre,50,stdin);
-		//scanf("%s",recepcion[a].nombre);
-	 	//fflush(stdin);
+		getchar();
+		fgets(recepcion[a].nombre,100,stdin);
 
 		printf("\nIntroduzca el numero de item: ");
 	 	scanf("%d",&recepcion[a].numArticulo);
@@ -186,131 +168,122 @@ void addproducts(int a){
 
 		//fgets(recepcion[a].presentacion,sizeof(recepcion[a].presentacion),stdin);
 		scanf("%s",recepcion[a].presentacion);
-	 	fflush(stdin);
 
 		printf("\nCantidad recibida: ");
 	 	scanf("%d",&recepcion[a].cantidadRecibida);
 
-		fflush(stdin);
-
  		if(strcmp("caja",recepcion[a].presentacion)==0 || strcmp("Caja",recepcion[a].presentacion)==0 ||strcmp("CAJA",recepcion[a].presentacion)==0){
 			printf("\nPiezas por caja: ");
 			scanf("%d",&recepcion[a].piezaPorCaja);
-
-			fflush(stdin);
 
 			recepcion[a].existencia = recepcion[a].cantidadRecibida*recepcion[a].piezaPorCaja;
 
 			printf("\nIntroduzca el precio del producto(por caja): ");
 			scanf("%f",&recepcion[a].precioporcaja);
 
-			fflush(stdin);
-
 			recepcion[a].precioUnitario = recepcion[a].precioporcaja/recepcion[a].piezaPorCaja;
 
 			printf("\nIntroduzca la fecha de recepcion: ");
-			do{
+
+			do {
 				printf("\nDia: ");
 				scanf("%d",&recepcion[a].frecepcion.dia);
-			}while(recepcion[a].frecepcion.dia>31 || recepcion[a].frecepcion.dia<1);
+			} while(recepcion[a].frecepcion.dia > 31 || recepcion[a].frecepcion.dia < 1);
 
-			do{
+			do {
 				printf("\nMes: ");
 				scanf("%d",&recepcion[a].frecepcion.mes);
-			}while(recepcion[a].frecepcion.mes>12 || recepcion[a].frecepcion.mes<1);
+			} while(recepcion[a].frecepcion.mes > 12 || recepcion[a].frecepcion.mes < 1);
 
 			//do{
-				printf("\nAnio: ");
-				scanf("%d",&recepcion[a].frecepcion.anio);
+			printf("\nAnio: ");
+			scanf("%d",&recepcion[a].frecepcion.anio);
 			//}while(recepcion[a].frecepcion.anio>31);
 
-			do{
+			do {
 				printf("\nHora: ");
-				scanf("%d",&recepcion[a].frecepcion.hora);
-			}while(recepcion[a].frecepcion.hora>24 || recepcion[a].frecepcion.hora<0);
+				scanf("%d", &recepcion[a].frecepcion.hora);
+			} while(recepcion[a].frecepcion.hora > 24 || recepcion[a].frecepcion.hora < 0);
 
-			do{
+			do {
 				printf("\nMinuto: ");
 				scanf("%d",&recepcion[a].frecepcion.minuto);
-			}while(recepcion[a].frecepcion.minuto>59 || recepcion[a].frecepcion.minuto<0);
+			} while(recepcion[a].frecepcion.minuto > 59 || recepcion[a].frecepcion.minuto < 0);
 
-			do{
+			do {
 				printf("\nSegundo: ");
-				scanf("%d",&recepcion[a].frecepcion.segundo);
-			}while(recepcion[a].frecepcion.segundo>59 || recepcion[a].frecepcion.segundo<0);
+				scanf("%d", &recepcion[a].frecepcion.segundo);
+			} while(recepcion[a].frecepcion.segundo > 59 || recepcion[a].frecepcion.segundo < 0);
 
-			fwrite(&recepcion[a],sizeof(recepcion),1,ptrFile);
-
+			fwrite(&recepcion[a], sizeof(recepcion), 1, ptrFile);
 	 } else {
 			printf("\nIntroduzca el precio del producto: ");
-			scanf("%f",&recepcion[a].precioUnitario);
+			scanf("%f", &recepcion[a].precioUnitario);
 			fflush(stdin);
 
 			printf("\nIntroduzca la fecha de recepcion: ");
-			do{
+			do {
 				printf("\nDia: ");
 				scanf("%d",&recepcion[a].frecepcion.dia);
-			}while(recepcion[a].frecepcion.dia>31 || recepcion[a].frecepcion.dia<1);
+			} while(recepcion[a].frecepcion.dia > 31 || recepcion[a].frecepcion.dia < 1);
 
-			do{
+			do {
 				printf("\nMes: ");
-				scanf("%d",&recepcion[a].frecepcion.mes);
-			}while(recepcion[a].frecepcion.mes>12 || recepcion[a].frecepcion.mes<1);
+				scanf("%d", &recepcion[a].frecepcion.mes);
+			} while(recepcion[a].frecepcion.mes > 12 || recepcion[a].frecepcion.mes < 1);
 
 			//do{
-				printf("\nAnio: ");
-				scanf("%d",&recepcion[a].frecepcion.anio);
+			printf("\nAnio: ");
+			scanf("%d",&recepcion[a].frecepcion.anio);
 			//}while(recepcion[a].frecepcion.anio>31);
 
-			do{
+			do {
 				printf("\nHora: ");
-				scanf("%d",&recepcion[a].frecepcion.hora);
-			}while(recepcion[a].frecepcion.hora>24 || recepcion[a].frecepcion.hora<0);
+				scanf("%d", &recepcion[a].frecepcion.hora);
+			} while(recepcion[a].frecepcion.hora > 24 || recepcion[a].frecepcion.hora < 0);
 
-			do{
+			do {
 				printf("\nMinuto: ");
-				scanf("%d",&recepcion[a].frecepcion.minuto);
-			}while(recepcion[a].frecepcion.minuto>59 || recepcion[a].frecepcion.minuto<0);
+				scanf("%d", &recepcion[a].frecepcion.minuto);
+			} while(recepcion[a].frecepcion.minuto > 59 || recepcion[a].frecepcion.minuto < 0);
 
 			do{
 				printf("\nSegundo: ");
-				scanf("%d",&recepcion[a].frecepcion.segundo);
-			}while(recepcion[a].frecepcion.segundo>59 || recepcion[a].frecepcion.segundo<0);
+				scanf("%d", &recepcion[a].frecepcion.segundo);
+			}while(recepcion[a].frecepcion.segundo > 59 || recepcion[a].frecepcion.segundo < 0);
 
 			//THIS PART ITS GOING TO SAVE THE DATA IN THE SALES STRUCTURE FOR CREATE AN ACUMULATE INVENTORY
 			//THE OTHER ARRAY ONE WILL BE SAVE THE HISTORY OF RECEPTIONS.
 
-			for(i=0;i==a;i++){
-				mark=strcmp(recepcion[a].nombre,ventas[i].nombre);
+			for(i=0; i==a; i++) {
+				mark = strcmp(recepcion[a].nombre, ventas[i].nombre);
 				flag++;
-				if(mark==0)
+				if(mark==0){
 					break;
+				}
 			}
 
-			if(mark==0){
-				strcpy(ventas[a].nombre,recepcion[a].nombre);
+			if (mark == 0) {
+				strcpy(ventas[a].nombre, recepcion[a].nombre);
 				ventas[flag].numArticulo = recepcion[a].numArticulo;
 				ventas[flag].existencia = ventas[flag].existencia+recepcion[a].existencia;
 				ventas[flag].precioporcaja = recepcion[a].precioporcaja;
 				ventas[flag].precioUnitario = recepcion[a].precioUnitario;
 				ventas[flag].piezaPorCaja = recepcion[a].piezaPorCaja;
-			}
-
-
-			else{
-				strcpy(ventas[a].nombre,recepcion[a].nombre);
+			} else {
+				strcpy(ventas[a].nombre, recepcion[a].nombre);
 				ventas[a].numArticulo = recepcion[a].numArticulo;
 				ventas[a].existencia = recepcion[a].existencia;
 				ventas[a].precioporcaja = recepcion[a].precioporcaja;
 				ventas[a].precioUnitario = recepcion[a].precioUnitario;
 				ventas[a].piezaPorCaja = recepcion[a].piezaPorCaja;
-
 			}
 
 			fwrite(&recepcion[a],sizeof(recepcion),1,ptrFile);
+		}
 
-		} fclose(ptrFile);
- } else {
+		fclose(ptrFile);
+ 	} else {
   	printf("Error en la apertura del archivo");
 	}
 	printf("\n");
@@ -318,17 +291,13 @@ void addproducts(int a){
 
 
 void downproducts(int a){
-
 	FILE *ptrFile;
 	ptrFile = fopen("papeleria.dat","ab");
 
-
-		if(ptrFile!=NULL){
-			printf("\nIngrese el nombre del articulo que se dara de baja...:");
-			fgets(bajas[a].nombre,50,stdin);
-
-
-	fclose(ptrFile);
+	if(ptrFile != NULL) {
+		printf("\nIngrese el nombre del articulo que se dara de baja...:");
+		fgets(bajas[a].nombre, 50, stdin);
+		fclose(ptrFile);
 	} else {
   	printf("Error en la apertura del archivo");
 	}
